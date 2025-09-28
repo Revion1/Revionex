3eb357495bbe2f72272451b5bc90f5ed63bd370a
Revion1 <hajoumishak5@gmail.com>
2025-09-28 10:33:03 +0100

    Add files via upload

--- new.js
+++ new.js
@@ -1,22 +1,14 @@
-var channelID = "UCHAdpRszgvVvLk3y-gMUBKg";
-var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=" + channelID;
-
-$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL), function(data) {
-    var link = data.items[0].link;
-    var id = link.substr(link.indexOf("=") + 1);
-    $("#youtube_video").attr("src", "https://www.youtube.com/embed/" + id + "?controls=0&showinfo=0&rel=0");
-});
 document.addEventListener("DOMContentLoaded", function () {
-  const para = document.getElementById("myPara");
+  const para = document.querySelector(".e");
 
   function checkVisibility() {
     const rect = para.getBoundingClientRect();
     if (rect.top < window.innerHeight && rect.bottom > 0) {
-      para.classList.add("show");
-      window.removeEventListener("scroll", checkVisibility);
+      para.classList.add("show"); // يضيف الكلاس ويشغل الأنيميشن
+      window.removeEventListener("scroll", checkVisibility); // يشيل المراقبة بعد التشغيل
     }
   }
 
   window.addEventListener("scroll", checkVisibility);
-  checkVisibility();
+  checkVisibility(); // باش يتحقق حتى إذا كان العنصر باين من الأول
 });
